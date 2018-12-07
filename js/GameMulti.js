8f9f4169659113b8eda11e6ec3433d00e9195df6
junhaddi <rkdwnsgk05@gmail.com>
2018-12-07 18:59:40 +0900

    Add multiplay ball

--- js/GameMulti.js
+++ js/GameMulti.js
@@ -1,10 +1,89 @@
 var socket;     
+var isConnected = false;
+var isBalled = false;
+
+var oPlayerList = [];
+
+var player, ball;
 
 //  접속 완료
 function onConnected() {
+    isConnected = true;
+    player = game.add.sprite(getRandomInt(0, CANVAS_WIDTH), getRandomInt(0, CANVAS_HEIGHT), chr_sprite[0]);
+    player.anchor.setTo(0.5,0.5);
+    socket.emit("new_player", { x: player.x, y: player.y, sprite: chr_sprite[0], radius: player.width, speed: 20 });
+
     console.log("Connected to server");
 }
 
+//  외부 플레이어 클래스
+var Player = function(id, startX, startY, sprite) {
+    this.id = id;
+    this.player = game.add.sprite(startX, startY, sprite);
+    this.player.anchor.setTo(0.5,0.5);
+}
+
+//  외부 플레이어 생성
+function onNewPlayer(data) {
+    var new_player = new Player(data.id, data.x, data.y, data.sprite);
+    oPlayerList.push(new_player);
+}
+
+//  내 플레이어 위치 받기
+function onInputRecieved(data) {
+    player.x = data.x;
+    player.y = data.y;
+}
+
+//  외부 플레이어 이동
+function onMovePlayer(data) {         
+    var movePlayer = find_playerID(data.id); 
+	if (!movePlayer) {
+		return;
+    }
+	movePlayer.player.x = data.x;
+    movePlayer.player.y = data.y;
+}
+
+//  플레이어 제거
+function onRemovePlayer(data) {
+    var removePlayer = find_playerID(data.id);
+	if (!removePlayer) {
+		return;
+	}
+	removePlayer.player.destroy();
+	oPlayerList.splice(oPlayerList.indexOf(removePlayer), 1);
+}
+
+//  볼 생성
+function onCreateBall(data) {
+    ball = game.add.sprite(data.x, data.y, 'spr_ball');
+    ball.angle = data.angle
+    ball.anchor.setTo(0.5,0.5);
+
+    isBalled = true;
+}
+
+//  볼 업데이트
+function onUpdateBall(data) {
+    if (isBalled == true) {
+        ball.x = data.x;
+        ball.y = data.y;
+        ball.angle = data.angle;
+        console.log(ball.x + " | " + ball.y);
+    }
+}
+
+//  플레이어 ID 찾기
+function find_playerID(id) {
+    for (var i = 0; i < oPlayerList.length; i++) {
+        if (oPlayerList[i].id == id) {
+            return oPlayerList[i]; 
+        }
+    }
+}
+
+//  main
 var gameMulti = {
     init: function() {
         socket = io();
@@ -12,9 +91,25 @@ var gameMulti = {
     },
 
     create: function() {
-        socket.on("ping", function() {
-            console.log("pong");
-        });
+        this.cursors = game.input.keyboard.createCursorKeys();
+        
+        socket.on("connect", onConnected);
+        socket.on("remove_player", onRemovePlayer);
+        socket.on("new_oPlayer", onNewPlayer);
+        socket.on("input_recieved", onInputRecieved);
+        socket.on("move_oPlayer", onMovePlayer);
+        socket.on("createBall", onCreateBall);
+        socket.on("updateBall", onUpdateBall);
         console.log("Client started");
+    },
+
+    update: function() {
+        if (isConnected) {
+            //  움직이기
+            socket.emit("input_fired", {
+                hspd: (this.cursors.right.isDown - this.cursors.left.isDown),
+                vspd: (this.cursors.down.isDown - this.cursors.up.isDown) 
+            });
+        }
     },
 }
--- server.js
+++ server.js
@@ -1,18 +1,165 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+//	게임 서버
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const p2 = require('p2');
 
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/assets", express.static(__dirname + "/assets"));
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(80, function() {
-  console.log("Listening on port 80");
+	console.log("Listening on port 80");
 });
 
+//  플레이어 리스트
+var playerList = [];
+
+//  서버 물리 세계
+var world = new p2.World({
+    gravity : [0,0]
+});
+var lastTimeSeconds = (new Date).getTime();
+setInterval(function() {
+	var dt = (new Date).getTime() - lastTimeSeconds;
+	lastTimeSeconds = (new Date).getTime();
+	world.step(1 / 60, dt, 10);
+    
+    //  update
+    io.emit('updateBall', { x: ball.position[0], y: ball.position[1], angle: ball.angle });
+}, 1000/60);
+
+//	볼 설정
+var ball = new p2.Body({
+	mass: 1,
+	position: [300, 300],
+	angle: 0,
+	velocity: [0, 0],
+	angularVelocity: 0,
+	fixedRotation: false
+});
+ball.addShape(new p2.Circle({ radius: 16 }));
+world.addBody(ball);
+
+//  새로운 플레이어
+function onNewPlayer(data) {
+    var newPlayer = new Player(this.id, data.x, data.y, data.sprite, data.radius, data.speed);
+
+    //  플레이어 물리 적용
+    playerBody = new p2.Body({
+        mass: 1,
+        position: [newPlayer.x, newPlayer.y],
+        angle: 0,
+        velocity: [0, 0],
+        angularVelocity: 0,
+        fixedRotation: true
+    });
+    playerBody.addShape(new p2.Circle({ radius: newPlayer.radius }));
+    newPlayer.playerBody = playerBody;
+    world.addBody(newPlayer.playerBody);
+
+    //  플레이어 정보
+    var current_info = {
+        id: newPlayer.id, 
+        x: newPlayer.x,
+        y: newPlayer.y,
+        sprite: newPlayer.sprite,
+    }
+
+    //  접속된 플레이어 정보 가져오기
+    for (var i = 0; i < playerList.length; i++) {
+        existPlayer = playerList[i];
+        var player_info = {
+            id: existPlayer.id,
+            x: existPlayer.x,
+            y: existPlayer.y,
+            sprite: existPlayer.sprite,
+        };
+        this.emit('new_oPlayer', player_info);
+    }
+
+    //  나를 제외한 모든 소켓에게 나의 정보 전송
+    this.broadcast.emit('new_oPlayer', current_info);
+    playerList.push(newPlayer);
+
+    //  볼 정보 전송
+	io.emit('createBall', { x: ball.position[0], y: ball.position[0], angle: ball.angle });
+
+    console.log("created new player with id " + this.id);
+}
+
+//  연결 끊김
+function onDisconnect() {
+    var removePlayer = find_playerID(this.id);
+    if (removePlayer) {
+        playerList.splice(playerList.indexOf(removePlayer), 1);
+    }
+    this.broadcast.emit('remove_player', { id: this.id });
+    
+    console.log("disconnect player " + this.id);
+}
+
+//	입력
+function onInputFired(data) {
+    var movePlayer = find_playerID(this.id); 
+    if (!movePlayer || !movePlayer.isInputDelay) {
+        return;
+    }
+    //  입력 지연
+    setTimeout(function() { movePlayer.isInputDelay = true }, 10);
+    movePlayer.isInputDelay = false;
+
+    //  플레이어 이동
+    movePlayer.playerBody.velocity[0] = data.hspd * movePlayer.speed;
+    movePlayer.playerBody.velocity[1] = data.vspd * movePlayer.speed;
+    movePlayer.x = movePlayer.playerBody.position[0];
+    movePlayer.y = movePlayer.playerBody.position[1];
+
+    var info = {
+		x: movePlayer.x,
+		y: movePlayer.y
+    }
+    var movePlayerData = {
+        id: movePlayer.id,
+		x: movePlayer.x,
+		y: movePlayer.y
+    }
+	this.emit("input_recieved", info);
+	this.broadcast.emit('move_oPlayer', movePlayerData);
+}
+
+//  플레이어 ID 찾기
+function find_playerID(id) {
+	for (var i = 0; i < playerList.length; i++) {
+		if (playerList[i].id == id) {
+			return playerList[i]; 
+		}
+	}
+	return false; 
+}
+
+//#region main
 io.on('connection', function(socket) {
-  setInterval(function() { io.emit("ping") }, 1000);
+    socket.on('new_player', onNewPlayer);
+    socket.on('disconnect', onDisconnect);
+    socket.on('input_fired', onInputFired);
 })
+//#endregion
+
+//#region 클래스
+//  플레이어 클래스
+var Player = function(id, startX, startY, sprite, radius, speed) {
+    this.id = id;
+    this.x = startX;
+    this.y = startY;
+    this.sprite = sprite;
+    this.radius = radius;
+    this.speed = speed;
+
+    this.isInputDelay = true;
+}
+//#endregion
